4a6642ddc765463f12c353e92c53a4896a8cd12c
Paras <guptaparas039@gmail.com>
2020-10-01 00:32:13 +0530

    formatted code, easier to read and quik fix

--- project/app/urls.py
+++ project/app/urls.py
@@ -4,10 +4,11 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-path('', views.index,name="home")  ,  
-path("about", views.about,name="about")  ,  
-path("contact", views.contact,name="about")  ,  
-path("services", views.services,name="services")  ,path('index', views.index,name="home")  , 
-    path('login', views.loginuser,name="login"),
-path('logout', views.logoutuser,name="logout"),
+    path('', views.index, name="home"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="about"),
+    path("services", views.services, name="services"), path(
+        'index', views.index, name="home"),
+    path('login', views.loginuser, name="login"),
+    path('logout', views.logoutuser, name="logout"),
 ]
